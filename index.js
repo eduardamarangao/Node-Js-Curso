86154b07bdfc44795c7015f3dc78e2754ac45f7e
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-06 13:19:40 -0500

    Added new codes

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Seja bem-vindo ao meu app!");
+  res.sendFile(__dirname + "/html/index.html");
 });
 
 app.get("/sobre", function (req, res) {
-  res.send("Minha página sobre");
+  res.sendFile(__dirname + "/html/sobre.html");
 });
 
 app.get("/blog", function (req, res) {
-  res.send("Bem-vindo ao meu blog!");
+  res.sendFile(__dirname + "/html/blog.html");
 });
 
 app.get("/ola/:cargo/:nome/:cor", function (req, res) {
