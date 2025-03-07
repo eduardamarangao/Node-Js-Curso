43a111d42634eb5209aa7065b355864edc9816e9
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-07 15:08:41 -0500

    Added new codes

--- teste.js
+++ teste.js
@@ -1,31 +1,29 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize('teste', 'root', '269824eam', {
-    host: "localhost", //rodando mysql no meu computador
-    dialect: 'mysql' //tipo de banco de dados que quero me conectar
-})
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("teste", "root", "269824eam", {
+  host: "localhost", //rodando mysql no meu computador
+  dialect: "mysql", //tipo de banco de dados que quero me conectar
+});
 
-const Postagem = sequelize.define('postagens', {
-    titulo: {
-        type: Sequelize.STRING
-    },
-    conteudo: {
-        type: Sequelize.TEXT
-    }
-})
+const Postagem = sequelize.define("postagens", {
+  titulo: {
+    type: Sequelize.STRING,
+  },
+  conteudo: {
+    type: Sequelize.TEXT,
+  },
+});
 
-const Usuario = sequelize.define('usuarios', {
-    nome: {
-        type: Sequelize.STRING
-    },
-    sobrenome: {
-        type: Sequelize.STRING
-    },
-    idade: {
-        type: Sequelize.INTEGER
-    },
-    email: {
-        type: Sequelize.STRING
-    }
-})
-
-Usuario.sync({force: true})
+const Usuario = sequelize.define("usuarios", {
+  nome: {
+    type: Sequelize.STRING,
+  },
+  sobrenome: {
+    type: Sequelize.STRING,
+  },
+  idade: {
+    type: Sequelize.INTEGER,
+  },
+  email: {
+    type: Sequelize.STRING,
+  },
+});
