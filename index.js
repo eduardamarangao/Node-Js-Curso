f37a7c43da6a0d65e798d16be7a0d9bdd0628cf9
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-07 15:27:57 -0500

    Added new codes

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize("teste", "root", "269824eam", {
+  host: "localhost", 
+  dialect: "mysql"
+});
 
   // Config
     // Template Engine
-    app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+    app.engine("handlebars", handlebars({ defaultLayout: "main" })); //definindo o layout padrão
     app.set("view engine", "handlebars");
 
   // Conexão com o banco de dados
