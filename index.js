2738deb940977bb6dafaefd44c64f5463f511f76
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-07 15:21:05 -0500

    Added new codes

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require("express");
 const app = express();
 const handlebars = require("express-handlebars");
 
-app.listen(8081, function () {
-  console.log("Servidor rodando na url http://localhost:8081");
+  // Config
+    // Template Engine
+    app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+    app.set("view engine", "handlebars");
+
+  // Conexão com o banco de dados
+  app.listen(8081, function () {
+    console.log("Servidor rodando na url http://localhost:8081");
 });
