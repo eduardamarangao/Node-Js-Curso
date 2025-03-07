f78b6e7877316d012f2baf95764cd3be15171a34
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-07 15:36:27 -0500

    Added new codes

--- index.js
+++ index.js
@@ -6,13 +6,18 @@ const Sequelize = require("sequelize");
 
   // Config
     // Template Engine
-    app.engine("handlebars", handlebars({ defaultLayout: "main" })); //definindo o layout padrão
-    app.set("view engine", "handlebars");
+    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
+    app.set('view engine', 'handlebars');
     // Conexão com o banco de dados MySql
     const sequelize = new Sequelize("teste", "root", "269824eam", {
       host: "localhost", 
       dialect: "mysql"
     });
+
+  // Rotas
+    app.get("/cad", (req, res) => {
+      res.send('<h1>Rota de cadastro de posts</h1>');
+    })
 
   // Conexão com o banco de dados
   app.listen(8081, function () {
