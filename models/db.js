50e161ae1e16e85d697a91849cc1fd1cc7a0a2e1
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-13 15:39:35 -0400

    Added new codes

--- models/db.js
+++ models/db.js
@@ -4,3 +4,8 @@ const sequelize = new Sequelize("teste", "root", "269824eam", {
     host: "localhost", 
     dialect: "mysql"
   });
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}
