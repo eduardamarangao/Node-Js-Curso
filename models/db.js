1507fcbee95412656e271ac8120ffac046eb5b60
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-16 15:10:29 -0400

    Added new codes

--- models/db.js
+++ models/db.js
@@ -2,7 +2,8 @@ const Sequelize = require("sequelize");
 // Conexão com o banco de dados MySql
 const sequelize = new Sequelize("postapp", "root", "269824eam", {
     host: "localhost", 
-    dialect: "mysql"
+    dialect: "mysql",
+    query:{raw:true}
   });
 
 module.exports = {
