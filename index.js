9670452f94ed07b8271774e79b09c178d48841cd
eduardamarangao <eduardaamorimmarangao@gmail.com>
2025-03-20 19:12:55 -0400

    Added new codes

--- index.js
+++ index.js
@@ -5,51 +5,51 @@ const bodyParser = require("body-parser");
 const Post = require("./models/Post");
 const { ConnectionTimedOutError, where } = require("sequelize");
 
+// Config
+// Template Engine
+app.engine("handlebars", handlebars.engine({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+// Body Parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-  // Config
-    // Template Engine
-    app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}));
-    app.set('view engine', 'handlebars');
-    // Body Parser
-      app.use(bodyParser.urlencoded({extended: false}));
-      app.use(bodyParser.json());
-    
+// Rotas
 
-  // Rotas
-
-    app.get('/',(req, res) => {
-      Post.findAll({order: [['id', 'DESC']]}).then((posts) => {
-        console.log(posts);
-        res.render('home', {posts: posts});
-      })
-    })
-
-    app.get("/cad", (req, res) => {
-      res.render('formulario');
-    })
-
-    app.post('/add', (req, res) => {
-      Post.create({
-        titulo: req.body.titulo,
-        conteudo: req.body.conteudo
-      }).then(() =>{
-        res.redirect("/");  
-      }).catch((erro) => {
-        res.send("Erro ao criar o post: " + erro);
-      })
-    })
-
-    app.get('/deletar/:id', (req, res) => {
-      Post.destroy({where: {'id': req.params.id}}).then(() => {
-        res.render("postagem");
-      }).catch((erro) => {
-        res.send("Essa postagem não existe "+ erro);
-      })  
-    })
-    
-  // Conexão com o banco de dados
-  app.listen(8081, function () {
-    console.log("Servidor rodando na url http://localhost:8081");
+app.get("/", (req, res) => {
+  Post.findAll({ order: [["id", "DESC"]] }).then((posts) => {
+    console.log(posts);
+    res.render("home", { posts: posts });
+  });
 });
 
+app.get("/cad", (req, res) => {
+  res.render("formulario");
+});
 
+app.post("/add", (req, res) => {
+  Post.create({
+    titulo: req.body.titulo,
+    conteudo: req.body.conteudo,
+  })
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((erro) => {
+      res.send("Erro ao criar o post: " + erro);
+    });
+});
+
+app.get("/deletar/:id", (req, res) => {
+  Post.destroy({ where: { id: req.params.id } })
+    .then(() => {
+      res.render("postagem");
+    })
+    .catch((erro) => {
+      res.send("Essa postagem não existe " + erro);
+    });
+});
+
+// Conexão com o banco de dados
+app.listen(8081, function () {
+  console.log("Servidor rodando na url http://localhost:8081");
+});
